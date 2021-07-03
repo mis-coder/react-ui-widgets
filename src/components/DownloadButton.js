6daed01f122f5b012e0dce09be241abbc2917cba
Antra Verma <antraverma99@gmail.com>
2021-07-03 20:25:19 +0530

    download button complete

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -1,15 +1,9 @@
 import React, { useState } from "react";
 import styled, { keyframes, css } from "styled-components";
 
-const primaryColor = "#4bbe61";
+const primaryColor = "#FEDF4D";
 
-const sharedDisplayStyles = `
-display: flex;
-align-items: center;
-justify-content: center;
-`;
-
-
+//animations
 const hideRightWing = keyframes`
   100%{
     transform: rotate(0deg);
@@ -19,20 +13,31 @@ const hideRightWing = keyframes`
 
 const showSector = keyframes`
   100%{visibility: visible};
-`
+`;
+
+const dash = keyframes`
+100% {stroke-dashoffset: 0;}
+`;
+
+//Common styles
+const sharedDisplayStyles = `
+display: flex;
+align-items: center;
+justify-content: center;
+`;
 
 const sharedSectorStyles = css`
-position: absolute;
-height: 100%;
-width: 100%;
-background-color: ${primaryColor};
-clip-path:circle(50% at 0% 0%);
-visibility: hidden;
-animation: ${showSector} 0.6s forwards;
-`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: ${primaryColor};
+  clip-path: circle(50% at 0% 0%);
+  visibility: hidden;
+  animation: ${showSector} 1s forwards linear;
+`;
+
 const animateArrowStyles = css`
   transform: translateY(-20px);
-  transition: transform 0.5s;
   &:before,
   &:after {
     animation: ${hideRightWing} 0.8s forwards;
@@ -57,7 +62,7 @@ const Text = styled.span`
 const OuterCircle = styled.div`
   height: 150px;
   width: 150px;
-  background-color: #32333e;
+  background-color: transparent;
   border: 10px solid ${primaryColor};
   border-radius: 100%;
   cursor: pointer;
@@ -101,47 +106,61 @@ const InnerCircle = styled.div`
   background-color: transparent;
   ${({ isVisible }) => isVisible && showInnerCircle}
   position: relative;
-  `;
+`;
 
-
-  const Sector1 = styled.span`
- ${sharedSectorStyles}
+const Sector1 = styled.span`
+  ${sharedSectorStyles}
   transform: translate(50%, -50%) rotate(-90deg);
-  `
-  const Sector2 = styled.span`
+`;
+const Sector2 = styled.span`
   ${sharedSectorStyles}
   transform: translate(50%, 50%);
   animation-delay: 0.2s;
-  `
-  const Sector3 = styled.span`
+`;
+const Sector3 = styled.span`
   ${sharedSectorStyles}
-  transform: translate(-50%, 49%) rotate(90deg);
+  transform: translate(-50%, 50%) rotate(90deg);
   animation-delay: 0.4s;
-  `
+`;
 
-  const Sector4 = styled.span`
+const Sector4 = styled.span`
   ${sharedSectorStyles}
   transform: translate(-50%, -50%) rotate(-180deg);
   animation-delay: 0.6s;
-  `
+`;
 
 const WhiteCircle = styled.div`
   height: 20px;
   width: 20px;
+  z-index: 10;
+
   background-color: #fff;
   border-radius: 100%;
-  z-index: 10;
 `;
 
+const TickIcon = styled.svg`
+  fill: none;
+  z-index: 10;
+  & path {
+    stroke-dasharray: 1000;
+    stroke-dashoffset: 1000;
+    animation: ${dash} 5s linear forwards;
+  }
+`;
 const DownloadButton = () => {
   const [isDownloading, setisDownloading] = useState(false);
-  const [isCompleted, setIsCompleted] = useState(false);
+  const [isDownloaded, setIsDownloaded] = useState(false);
   const [isArrowHidden, setIsArrowHidden] = useState(false);
 
   const handleClick = () => {
     setisDownloading(true);
-    setTimeout(() => setIsArrowHidden(true), 
-    800);
+    setTimeout(() => {
+      setIsArrowHidden(true);
+      setTimeout(() => {
+        setisDownloading(false);
+        setIsDownloaded(true);
+      }, 1000);
+    }, 800);
   };
   return (
     <Wrapper>
@@ -152,10 +171,31 @@ const DownloadButton = () => {
           <Sector2 />
           <Sector3 />
           <Sector4 />
-          <WhiteCircle />
+          {isDownloading ? (
+            <WhiteCircle />
+          ) : (
+            <TickIcon
+              width="48"
+              height="52"
+              viewBox="0 0 38 42"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4 18.902L12.6808 34L34 4"
+                stroke="white"
+                strokeWidth="8"
+                strokeLinecap="round"
+              />
+            </TickIcon>
+          )}
         </InnerCircle>
       </OuterCircle>
-      <Text>{isDownloading ? "Downloading..." : "Download"}</Text>
+      {isDownloaded ? (
+        <Text>Downloaded!</Text>
+      ) : (
+        <Text>{isDownloading ? "Downloading..." : "Download"}</Text>
+      )}
     </Wrapper>
   );
 };
